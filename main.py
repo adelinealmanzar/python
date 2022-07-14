cc139491bde7771a3f265085ee4f78e2d5b1dc20
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-07-13 21:49:53 -0700

    ex: s4 logical operators

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 # exercise
-my_list = [1,2,3,4,5,5]
+is_magician = True
+is_expert = False
 
-print(set(my_list))
+#check if magicion AND expert: "you are a master magician"
+if is_magician and is_expert:
+    print("you are a master magician")
+#check if magician but not expert: "at least you're getting there"
+elif is_magician and not(is_expert):
+    print("at least you're getting there")
+#if you're not a magician: "you need magical powers"
+elif not(is_magician):
+    print("you need magical powers")

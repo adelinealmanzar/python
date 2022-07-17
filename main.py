ff517616f4e6e7e46d7cfbab3efb3a1bd523917a
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-07-17 16:49:41 -0700

    ex: enumerate

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
 # exercise
-
-#build counter to return sum total of list
-my_list = [1,2,3,4,5,6,7,8,9]
-
-sum = 0
-
-for num in my_list:
-    sum += num
-print(sum)
+for i, char in enumerate(list(range(50))):
+    if char == 20:
+        print(f'index of 20 is: {i}')

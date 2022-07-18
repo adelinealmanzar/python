4ceaeba8a0d2ca843987eb8c4171f57b72aed2ad
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-07-17 17:07:15 -0700

    ex: gui

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 # exercise
-for i, char in enumerate(list(range(50))):
-    if char == 20:
-        print(f'index of 20 is: {i}')
+
+#Display the image below to the right hand side where the 0 is going to be ' ', and the 1 is going to be '*'. This will reveal an image!
+picture = [
+  [0,0,0,1,0,0,0],
+  [0,0,1,1,1,0,0],
+  [0,1,1,1,1,1,0],
+  [1,1,1,1,1,1,1],
+  [0,0,0,1,0,0,0],
+  [0,0,0,1,0,0,0]
+]
+
+for image in picture:
+    for pixel in image:
+        if pixel == 1:
+            print('*', end='')
+        else:
+            print(' ', end='')
+    print('\n')

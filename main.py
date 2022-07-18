2d78ef35549f51cef8257153d7244adc2b565226
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-07-17 17:13:13 -0700

    ex: find duplicates

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
 # exercise
 
-#Display the image below to the right hand side where the 0 is going to be ' ', and the 1 is going to be '*'. This will reveal an image!
-picture = [
-  [0,0,0,1,0,0,0],
-  [0,0,1,1,1,0,0],
-  [0,1,1,1,1,1,0],
-  [1,1,1,1,1,1,1],
-  [0,0,0,1,0,0,0],
-  [0,0,0,1,0,0,0]
-]
+some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
 
-for image in picture:
-    for pixel in image:
-        if pixel == 1:
-            print('*', end='')
-        else:
-            print(' ', end='')
-    print('\n')
+duplicates = []
+
+
+for value in some_list:
+    if some_list.count(value) > 1:
+        if value not in duplicates:
+            duplicates.append(value)
+
+print(duplicates)

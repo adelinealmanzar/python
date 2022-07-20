ffba224989a4905eec8383f267f48e666759dc93
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-07-19 21:48:44 -0700

    ex: super()

--- main.py
+++ main.py
@@ -1,45 +1,12 @@
 # exercise
-class Pets():
-    animals = []
-    def __init__(self, animals):
-        self.animals = animals
 
-    def walk(self):
-        for animal in self.animals:
-            print(animal.walk())
-
-class Cat():
-    is_lazy = True
-
-    def __init__(self, name, age):
+class User():
+    def __init__(self, name):
         self.name = name
-        self.age = age
 
-    def walk(self):
-        return f'{self.name} is just walking around'
+class Wizard(User):
+    def __init__(self, name):
+        super().__init__(name)
 
-class Simon(Cat):
-    def sing(self, sounds):
-        return f'{sounds}'
-
-class Sally(Cat):
-    def sing(self, sounds):
-        return f'{sounds}'
-
-#1 Add another Cat
-class Sandra(Cat):
-    def sing(self, sounds):
-        return f'{sounds}'
-
-simon = Simon('simon', 10)
-sally = Sally('sally', 9)
-sandra = Sandra('sandra', 5)
-
-#2 Create a list of all of the pets (create 3 cat instances from the above)
-my_cats = [simon, sally, sandra]
-
-#3 Instantiate the Pet class with all your cats use variable my_pets
-pets = Pets(my_cats)
-#4 Output all of the cats walking using the my_pets instance
-for animal in pets.animals:
-    print(animal.walk())
+wizard = Wizard('me')
+print(wizard.name)

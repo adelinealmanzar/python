b8161e92fb4109b5d60d380feecac6f89da6e264
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-07-20 21:46:41 -0700

    ex: functional functions

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
+from functools import reduce
 # exercise
 
-class SuperList(list):
-    def __len__(self):
-        return 1000
+#1 Capitalize all of the pet names and print the list
+my_pets = ['sisi', 'bibi', 'titi', 'carla']
+def capitalize_item(item):
+    return item.capitalize()
+print(list(map(capitalize_item, my_pets)))
 
-super_list1 = SuperList()
 
-print(super_list1)
+#2 Zip the 2 lists into a list of tuples, but sort the numbers from lowest to highest.
+my_strings = ['a', 'b', 'c', 'd', 'e']
+my_numbers = [5,4,3,2,1]
+my_numbers.sort()
+print(list(zip(my_strings, my_numbers)))
+
+
+#3 Filter the scores that pass over 50%
+scores = [73, 20, 65, 19, 76, 100, 88]
+def over_50(item):
+    return item > 50
+print(list(filter(over_50, scores)))
+
+
+#4 Combine all of the numbers that are in a list on this file using reduce (my_numbers and scores). What is the total?
+combined_nums = my_numbers + scores
+def accumulator(acc, item):
+    return acc + item
+print(reduce(accumulator, combined_nums, 1))

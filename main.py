25b974bc409a6826667b69200f4d5cad91382f75
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-07-26 19:31:52 -0700

    ex: lambda square map

--- main.py
+++ main.py
@@ -1,23 +1,6 @@
 from functools import reduce
 # exercise
 
-#1 Capitalize all of the pet names and print the list
-my_pets = ['sisi', 'bibi', 'titi', 'carla']
-print(list(map(lambda item: item.capitalize(), my_pets)))
-
-
-#2 Zip the 2 lists into a list of tuples, but sort the numbers from lowest to highest.
-my_strings = ['a', 'b', 'c', 'd', 'e']
-my_numbers = [5,4,3,2,1]
-# my_numbers.sort()
-print(list(zip(my_strings, sorted(my_numbers))))
-
-
-#3 Filter the scores that pass over 50%
-scores = [73, 20, 65, 19, 76, 100, 88]
-print(list(filter(lambda item: item > 50, scores)))
-
-
-#4 Combine all of the numbers that are in a list on this file using reduce (my_numbers and scores). What is the total?
-combined_nums = my_numbers + scores
-print(reduce(lambda acc, item: acc + item, combined_nums))
+#square a list using a lambda expression
+my_list = [5,4,3]
+print(list(map(lambda el: el**2, my_list)))

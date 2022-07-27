21fe36a38c95b3ce95852a7790c094923d85a1eb
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-07-26 19:53:03 -0700

    ex: comprehension exercise using set

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from functools import reduce
 # exercise
 
-# dictionary comprehension
-my_dict = {num:num*2 for num in [1,2,3]}
+#return duplicates in list using a comprehension
+some_list = ['a','b','c','b','d','m','n','n']
+
+duplicates = []
+for value in some_list:
+    if some_list.count(value) > 1:
+        if value not in duplicates:
+            duplicates.append(value)
+
+
+comp_duplicates = list(set([el for el in some_list if some_list.count(el) > 1]))
+print(comp_duplicates)

05ceb7d08f41f58eebdc9bb69e9ddf782ae65265
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-07-26 19:34:47 -0700

    ex: lambda sorting

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ from functools import reduce
 #square a list using a lambda expression
 my_list = [5,4,3]
 print(list(map(lambda el: el**2, my_list)))
+
+# list sorting using a lambda expression
+a = [(0,2), (4,3), (9,9), (10,-1)]
+a.sort(key=lambda el: el[1])
+print(a)

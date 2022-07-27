e0f796c62954d1e22fcb3d70f91b8ee62c45775f
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-07-26 20:21:42 -0700

    ex: decorators args execise

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from time import time
 # exercise
 
-def performance(fn):
+# Create an @authenticated decorator that only allows the function to run if user1 has 'valid' set to True:
+user1 = {
+    'name': 'Sorna',
+    'valid': True #changing this will either run or not run the message_friends function.
+}
+
+def authenticated(fn):
     def wrapper(*args, **kwargs):
-        t1 = time()
-        result = fn(*args, **kwargs)
-        t2 = time()
-        print(f'took {t2-t1} ms')
-        return result
+        if args[0]['valid'] == True:
+            return fn(*args, **kwargs)
     return wrapper
 
-@performance
-def long_time():
-    for i in range(100):
-        i*5
+@authenticated
+def message_friends(user):
+    print('message has been sent')
 
-long_time()
+message_friends(user1)

8d02f3b28d463b40e0feec6f03ce85c4771e42cb
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-07-26 19:46:35 -0700

    ex: dict comprehension

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 from functools import reduce
 # exercise
 
-#square a list using a lambda expression
-my_list = [5,4,3]
-print(list(map(lambda el: el**2, my_list)))
-
-# list sorting using a lambda expression
-a = [(0,2), (4,3), (9,9), (10,-1)]
-a.sort(key=lambda el: el[1])
-print(a)
+# dictionary comprehension
+my_dict = {num:num*2 for num in [1,2,3]}

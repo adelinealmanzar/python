12e92f6ed3d8db9189ee688eb3f6c728f97afb3a
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-07-27 21:05:34 -0700

    ex: error handling v3

--- main.py
+++ main.py
@@ -4,9 +4,8 @@
 while True:
     try:
         age = int(input('what is your age'))
-        10/age
-        print(age)
-    except (ValueError, ZeroDivisionError) as err:
+        raise ValueError('look at me!')
+    except ZeroDivisionError as err:
         print(err)
     else: #else if we don't run into an error and the user enters valid information
         print('thank you!')

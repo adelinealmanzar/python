0f3a3122fd00e17dfe10580ce47a0865d72d4967
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-07-30 20:23:53 -0700

    ex: performance of generators

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 # exercise
 
-#generator
-def generator_func(num):
-    for i in range(num):
-        yield i
+#generator performance
+from time import time
 
-g = generator_func(100)
-next(g)
-next(g)
-print(next(g))
+
+def performance(func):
+    def wrap_func(*args,**kwargs):
+        t1 = time()
+        result = func(*args,**kwargs)
+        t2 = time()
+        print(t2-t1)
+        return result
+    return wrap_func
+
+@performance
+def long_time():
+    print('1')
+    for i in range(100):
+        i*5
+    
+@performance
+def long_time2():
+    print('2')
+    for i in list(range(100)):
+        i*5
+
+long_time()
+long_time2()

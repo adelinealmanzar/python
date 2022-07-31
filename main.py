8647263a2f612b7f0b04c2f7337409f8e937e8ed
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-07-30 20:15:01 -0700

    generators 101

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 # exercise
 
-#error handling
-while True:
-    try:
-        age = int(input('what is your age'))
-        raise ValueError('look at me!')
-    except ZeroDivisionError as err:
-        print(err)
-    else: #else if we don't run into an error and the user enters valid information
-        print('thank you!')
-        break
+#generator
+def generator_func(num):
+    for i in range(num):
+        yield i
+
+g = generator_func(100)
+next(g)
+next(g)
+print(next(g))

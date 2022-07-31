5c5c93090987bbd9d457efc4c2d16075f541c500
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-07-30 20:39:25 -0700

    ex: range generator

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 # exercise
 
-#generators under the hood
-def for_loop(iterable):
-    iterator = iter(iterable)
-    while True:
-        try:
-            print(iterator)
-            next(iterator)
-        except StopIteration:
-            break
+#range generator
 
-for_loop([1,2,3])
+class RangeGen():
+    current = 0
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if RangeGen.current < self.last:
+            num = RangeGen.current
+            RangeGen.current += 1
+            return num
+        raise StopIteration
+
+range_gen = RangeGen(0,100)
+for i in range_gen:
+    print(i)

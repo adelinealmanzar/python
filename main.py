91080e3e7e26da47d7019a12059a52242442c7ca
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-07-31 20:10:14 -0700

    ex: fib generator

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
 # exercise
 
-#range generator
+#fibonacci
+from curses import A_ALTCHARSET
 
-class RangeGen():
-    current = 0
-    def __init__(self, first, last):
-        self.first = first
-        self.last = last
-    
-    def __iter__(self):
-        return self
-    
-    def __next__(self):
-        if RangeGen.current < self.last:
-            num = RangeGen.current
-            RangeGen.current += 1
-            return num
-        raise StopIteration
 
-range_gen = RangeGen(0,100)
-for i in range_gen:
-    print(i)
+def fib(num):
+    a = 0
+    b = 1
+    for i in range(num):
+        yield a
+        temp = a
+        a = b
+        b = temp + b
+
+for x in fib(10):
+    print(x)

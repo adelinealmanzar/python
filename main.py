d01c3e90e135b06fe90dce0962399bd1d94b6664
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-07-31 20:33:57 -0700

    ex: 1/100 problems

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 # exercise
 
-#fibonacci
-def fib(num):
-    a = 0
-    b = 1
-    for i in range(num):
-        yield a
-        temp = a
-        a = b
-        b = temp + b
+#Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5, between 2000 and 3200 (both included).The numbers obtained should be printed in a comma-separated sequence on a single line.
+def div_by_7_not_5(num1, num2):
+    result = []
+    for i in range(num1, num2 + 1):
+        if i % 7 == 0 and i % 5 != 0:
+            result.append(i)
+    return result
 
-for x in fib(10):
-    print(x)
+print(div_by_7_not_5(2000,3200))

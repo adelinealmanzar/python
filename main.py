c2d60e004750f1138878d035bdd4929486e773e8
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-07-31 20:38:10 -0700

    ex: 2/100 problems

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 # exercise
 
-#Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5, between 2000 and 3200 (both included).The numbers obtained should be printed in a comma-separated sequence on a single line.
-def div_by_7_not_5(num1, num2):
-    result = []
-    for i in range(num1, num2 + 1):
-        if i % 7 == 0 and i % 5 != 0:
-            result.append(str(i))
-    return ','.join(result)
+# Write a program which can compute the factorial of a given numbers.The results should be printed in a comma-separated sequence on a single line.Suppose the following input is supplied to the program: 8 Then, the output should be:40320
+def fact(num):
+    if num == 0:
+        return 1
+    return num * fact(num-1)
 
-print(div_by_7_not_5(2000,3200))
+print(fact(8))

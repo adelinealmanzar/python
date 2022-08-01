fb62e885a43c426adbf77a8eb2c10ae0527743eb
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-07-31 20:42:13 -0700

    ex: 3/100 problems

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # exercise
 
-# Write a program which can compute the factorial of a given numbers.The results should be printed in a comma-separated sequence on a single line.Suppose the following input is supplied to the program: 8 Then, the output should be:40320
-def fact(num):
-    if num == 0:
-        return 1
-    return num * fact(num-1)
+# With a given integral number n, write a program to generate a dictionary that contains (i, i x i) such that is an integral number between 1 and n (both included). and then the program should print the dictionary.Suppose the following input is supplied to the program: 8
+def integral_dict(n):
+    dict = {}
+    for i in range(1, n + 1):
+        dict[i] = i * i
+    return dict
 
-print(fact(8))
+print(integral_dict(8))

9477562ad339127f0d02226b2266470e09559b10
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-08-01 20:50:17 -0700

    ex: 4/100 problems

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 # exercise
 
-# With a given integral number n, write a program to generate a dictionary that contains (i, i x i) such that is an integral number between 1 and n (both included). and then the program should print the dictionary.Suppose the following input is supplied to the program: 8
-def integral_dict(n):
-    dict = {}
-    for i in range(1, n + 1):
-        dict[i] = i * i
-    return dict
-
-print(integral_dict(8))
+# Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a tuple which contains every number.
+text = input('What is your comma-separated sequence of numbers?')
+text_list = text.split(',')
+text_tuple = tuple(text_list)
+print(text_list)
+print(text_tuple)

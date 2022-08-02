4c7d52be3a1de8f9c23b7e50ed8b1e75387e2753
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-08-01 20:54:39 -0700

    ex: 5/100 problems

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 # exercise
 
-# Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a tuple which contains every number.
-text = input('What is your comma-separated sequence of numbers?')
-text_list = text.split(',')
-text_tuple = tuple(text_list)
-print(text_list)
-print(text_tuple)
+# Define a class which has at least two methods: 1. getString and 2.printString
+
+class InputOutString():
+    def __init__(self):
+        self.s = ""
+    def getString(self):
+        self.s = input()
+    def printString(self):
+        print(self.s.upper())
+
+ioString = InputOutString()
+ioString.getString()
+ioString.printString()

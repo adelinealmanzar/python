099f6a45beac4f767260c7d5ee7c879522918e38
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-08-11 13:49:26 -0700

    ex: 7/100 problems

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 # exercise
 
-# Write a program that calculates and prints the value according to the given formula:
-# Q = Square root of [(2 _ C _ D)/H]
-# Following are the fixed values of C and H:
-# C is 50. H is 30.
-# D is the variable whose values should be input to your program in a comma-separated sequence.For example Let us assume the following comma separated input sequence is given to the program:
+# _Write a program which takes 2 digits, X,Y as input and generates a 2-dimensional array. The element value in the i-th row and j-th column of the array should be i _ j.*
 
-from math import sqrt
+x,y = map(int,input().split(','))
+print(x)
+lst = []
+for i in range(x):
+    row = []
+    for j in range(y):
+        row.append(i*j)
+    lst.append(row)
 
-C,H = 50,30
-
-def calc(D):
-    return sqrt((2*C*D)/H)
-
-D = input().split(',')
-D = [str(round(calc(int(i)))) for i in D]
-print(",".join(D))
+print(lst)

dc455d7e99114514db903c95eb399f045335b12a
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-08-15 16:42:00 -0700

    ex: 12/100 problems

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 # exercise
 
-# Write a program which accepts a sequence of comma separated 4 digit binary numbers as its input and then check whether they are divisible by 5 or not. The numbers that are divisible by 5 are to be printed in a comma separated sequence.
+# Write a program, which will find all such numbers between 1000 and 3000 (both included) such that each digit of the number is an even number.The numbers obtained should be printed in a comma-separated sequence on a single line.result = []
 result = []
-nums = input('Tell me your binary numbers').split(',')
-for num in nums:
-    if int(num, 2) % 5 == 0:
-        result.append(num)
-
+for num in range(1000, 3001):
+    if num % 2 == 0:
+        result.append(str(num))
 print(','.join(result))

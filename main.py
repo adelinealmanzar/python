c95c732f4261bd9ae0a2a1c85cbcfb412f2a010e
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-08-15 16:34:43 -0700

    ex: 10/100 problems

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
 # exercise
 
-# Write a program that accepts sequence of lines as input and prints the lines after making all characters in the sentence capitalizedwords = input().split(',')
-lines = []
+# Write a program that accepts a sequence of whitespace separated words as input and prints the words after removing all duplicate words and sorting them alphanumericallylines = []
 
-while True:
-    s = input('What are your phases?')
-    if s:
-        lines.append(s.upper())
-    else:
-        break
-
-for sentence in lines:
-    print(sentence)
+words = input('Tell me your words:').split(' ')
+words.sort()
+print(set(words))

a9f4f745776c082cb42947e1e74e1efe0beda82d
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-08-16 12:10:30 -0700

    ex: 13/100 problems v2

--- main.py
+++ main.py
@@ -6,13 +6,12 @@ text = input('Letters and Digiits plaase:')
 letters = 0
 digits = 0
 for char in text:
-    print(digits, letters)
-    if char.isdigit():
-        print('here')
+    if ('a' <= char and char <= 'z') or ('A' <= char and char <= 'Z'):
         # if it's a digit
-        digits += 1
-    elif char.isalpha():
-        #if it's a letter
         letters += 1
+    elif '0' <= char and char <='9':
+        #if it's a letter
+        digits += 1
+
 
 print(f'DIGITS {digits}\nLETTERS {letters}')  

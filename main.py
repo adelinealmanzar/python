13959d77c747a0ba8894e6da045fa6a92549d006
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-08-17 16:08:39 -0700

    ex: 14/100 problems

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 # exercise
 
 # Write a program that accepts a sentence and calculate the number of letters and digitsresult = []
-text = input('Letters and Digits please:')
+text = input('Sentence please:')
 
-letters = 0
-digits = 0
+uppercase = 0
+lowercase = 0
+
 for char in text:
-    if ('a' <= char and char <= 'z') or ('A' <= char and char <= 'Z'):
-        # if it's a digit
-        letters += 1
-    elif '0' <= char and char <='9':
-        #if it's a letter
-        digits += 1
+    if char.isupper():
+        uppercase += 1
+    elif char.isalpha():
+        lowercase += 1
+        
 
-
-print(f'DIGITS {digits}\nLETTERS {letters}')  
+print(f'UPPER CASE {uppercase}\nLOWER CASE {lowercase}')

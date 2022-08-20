924972bc94af9bc3ebab6a9ea113adfb9c070168
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-08-19 18:42:29 -0700

    ex: 17/100 problems

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 # exercise
 
-# Use a list comprehension to square each odd number in a list. The list is input by a sequence of comma-separated numbers. >Suppose the following input is supplied to the program:
-# newlist = [x for x in fruits if "a" in x]
+# Write a program that computes the net amount of a bank account based a transaction log from console input.
 
-nums = input('Numbers, please:').split(',')
-squared_odd_nums = [int(num) ** 2 for num in nums if int(num) % 2 == 1]
-print(squared_odd_nums)
+input_log = input('Transaction Log:').split(" ")
+
+balance = 0
+for i, el in enumerate(input_log):
+    if el == "D":
+        balance += int(input_log[i + 1])
+    elif el == "W":
+        balance -= int(input_log[i + 1])
+
+print(balance)

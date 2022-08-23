8f49ececc0ad6038d8e52285af87ddf0e3f7f22f
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-08-22 19:26:20 -0700

    ex: 18/100 problems

--- main.py
+++ main.py
@@ -1,14 +1,42 @@
 # exercise
 
-# Write a program that computes the net amount of a bank account based a transaction log from console input.
+# A website requires the users to input username and password to register. Write a program to check the validity of password input by users.
+passwords = input('Passwords:').split(",")
 
-input_log = input('Transaction Log:').split(" ")
+def is_in_char_limit(word):
+    if len(word) > 6 and len(word) < 12:
+        return True
+    return False
 
-balance = 0
-for i, el in enumerate(input_log):
-    if el == "D":
-        balance += int(input_log[i + 1])
-    elif el == "W":
-        balance -= int(input_log[i + 1])
+def is_lowercase(word):
+    for l in word:
+        if l.islower() == True:
+            return True
+    return False
 
-print(balance)
+def is_uppercase(word):
+    for l in word:
+        if l.isupper() == True:
+            return True
+    return False
+
+def is_number(word):
+    for l in word:
+        if l.isnumeric() == True:
+            return True
+    return False
+
+def is_special(word):
+    for l in word:
+        if l == '@' or l == '#' or l == '$':
+            return True
+    return False
+
+def check_passwords(passwords):
+    result = []
+    for password in passwords:
+        if is_in_char_limit(password) and is_lowercase(password) and is_uppercase(password) and is_number(password) and is_special(password):
+            result.append(password)
+    return result
+
+print(check_passwords(passwords))

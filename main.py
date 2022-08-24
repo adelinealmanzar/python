d0fcfa95bfc23cebaf17bdd332d95f1c01a68f9f
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-08-24 10:08:41 -0700

    ex: 20/100 problems fixed

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 # exercise
 
 # Define a class with a generator which can iterate the numbers, which are divisible by 7, between a given range 0 and n
-from tkinter import N
-
-
 class Divisible():
     def by_7(self, n):
         for num in range(0, n + 1):

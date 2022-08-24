4b61aa96c50959ef939ecdc4092d8995f08733f6
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-08-24 10:51:55 -0700

    ex: 21/100 problems

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+import math
 # exercise
 
-# Define a class with a generator which can iterate the numbers, which are divisible by 7, between a given range 0 and n
-class Divisible():
-    def by_7(self, n):
-        for num in range(0, n + 1):
-            if num % 7 == 0:
-                yield num
+# A robot moves in a plane starting from the original point (0,0). The robot can move toward UP, DOWN, LEFT and RIGHT with a given steps.# The numbers after the direction are steps. Please write a program to compute the distance from current position after a sequence of movement and original point. If the distance is a float, then just print the nearest integer.
+# The numbers after the direction are steps. Please write a program to compute the distance from current position after a sequence of movement and original point. If the distance is a float, then just print the nearest integer.
+steps_list = input('What are the up, down, left, and right steps taken?').split(';')
 
-for i in Divisible().by_7( int(input('Please enter a number:'))):
-    print(i)
+def euclidean_dist(x,y):
+   # original position assumed to always start at 0 
+    dist = math.sqrt(y**2 + x**2)
+    return round(dist)
+
+def abs_dist():
+    steps_dict = dict(x.split(" ") for x in steps_list)
+    x = abs(int(steps_dict['LEFT']) - int(steps_dict['RIGHT']))
+    y = abs(int(steps_dict['UP']) - int(steps_dict['DOWN']))
+    return euclidean_dist(x,y)
+
+
+print(abs_dist())
+

6970727f64de51a891963a58d01dc869a89f30bd
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-08-26 12:18:39 -0700

    ex: 24/100 problems

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 # exercise
 
-# Write a method which can calculate square value of number
-def square(n):
-    return n**2
+# Python has many built-in functions, and if you do not know how to use it, you can read document online or find some books. But Python has a built-in document function for every built-in functions
+print('----------')
+print("abs:", abs.__doc__)
+print('----------')
+print("int:", int.__doc__)
+print('----------')
+print("sorted:", sorted.__doc__)

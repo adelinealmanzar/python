c63acff369e81f41323bd3801b8e92f06ab0582e
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-08-25 20:32:14 -0700

    ex: 23/100 problems

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 # exercise
 
-# Write a program to compute the frequency of the words from the input. The output should output after sorting the key alphanumerically.
-
-words = input('What is your input sentence?').split(' ')
-words.sort()
-
-freq = {}
-
-for el in words:
-    freq[el] = freq.get(el, 0) + 1
-
-
-for key in freq:
-    print(f'{key}:{freq[key]}')
-    
+# Write a method which can calculate square value of number
+def square(n):
+    return n**2

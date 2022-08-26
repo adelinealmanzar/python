62ccbd058c2a909cac5837af1013ebceb3f05ba2
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-08-25 20:18:02 -0700

    ex: 22/100 problems

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
-import math
 # exercise
 
-# A robot moves in a plane starting from the original point (0,0). The robot can move toward UP, DOWN, LEFT and RIGHT with a given steps.# The numbers after the direction are steps. Please write a program to compute the distance from current position after a sequence of movement and original point. If the distance is a float, then just print the nearest integer.
-# The numbers after the direction are steps. Please write a program to compute the distance from current position after a sequence of movement and original point. If the distance is a float, then just print the nearest integer.
-steps_list = input('What are the up, down, left, and right steps taken?').split(';')
+# Write a program to compute the frequency of the words from the input. The output should output after sorting the key alphanumerically.
 
-def euclidean_dist(x,y):
-   # original position assumed to always start at 0 
-    dist = math.sqrt(y**2 + x**2)
-    return round(dist)
+words = input('What is your input sentence?').split(' ')
+words.sort()
 
-def abs_dist():
-    steps_dict = dict(x.split(" ") for x in steps_list)
-    x = abs(int(steps_dict['LEFT']) - int(steps_dict['RIGHT']))
-    y = abs(int(steps_dict['UP']) - int(steps_dict['DOWN']))
-    return euclidean_dist(x,y)
+freq = {}
+
+for el in words:
+    freq[el] = freq.get(el, 0) + 1
 
 
-print(abs_dist())
-
+for key in freq:
+    print(f'{key}:{freq[key]}')
+    

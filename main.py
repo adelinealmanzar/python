f823e241b313c853d69deb00fb93b070c887b492
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-08-26 12:27:16 -0700

    ex: 26/100 problems v1

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 # exercise
 
-# Define a class, which have a class parameter and have a same instance parameter.
-class Animal():
-    animal_type = 'Animal'
+# Define a function which can compute the sum of two numbers.
+def sum(n1, n2):
+    return n1 + n2
 
-    def __init__(self, animal_type = None):
-        self.animal_type = animal_type
-
-a = Animal('Dog')
-print(a.animal_type) #=> 'Dog'
-print(Animal.animal_type) #=> 'Animal'
-print(Animal().animal_type) #=> None
+print(sum(10, 5))

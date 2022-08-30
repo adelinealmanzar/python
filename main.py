0a4da2ee9eec4042edf193fe660c0c1390ca1517
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-08-30 11:20:46 -0700

    ex: 28/100 problems v2 lambda

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 
 # Define a function that can receive two integer numbers in string form and compute their sum and then print it in console
 
-def strs_sum(s1, s2):
-    sum = int(s1) + int(s2)
-    print(sum)
+strs_sum = lambda s1, s2: int(s1) + int(s2)
 
-strs_sum('10', '12')
+print(strs_sum('10', '10'))
